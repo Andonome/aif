2c9c5080ed6337fa8aef7a5aa7dc26b1c2fd684c
Malin Freeborn <malinfreeborn@tutamail.com>
2019-11-22 18:49:03 +0100

    add auto sync with config to autocompile

--- autocompile.sh
+++ autocompile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-git subtree -P config pull https://gitlab.com/FirstBloodRPG/config master
-git subtree -P config push https://gitlab.com/FirstBloodRPG/config master
+[ -e ../config ] || git clone https://gitlab.com/FirstBloodRPG/config  ..
+git subtree -P config pull ../clone master
+git subtree -P config push ../clone aif
 pdflatex -shell-escape main.tex
 makeglossaries main
 makeglossaries main.idx
