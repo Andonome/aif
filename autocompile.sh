3a4269cdabb387be09e5eb1f2906e852bfa9a6af
Malin Freeborn <malinfreeborn@tutamail.com>
2019-11-26 06:02:47 +0100

    correct autocompile script

--- autocompile.sh
+++ autocompile.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-[ -e ../config ] || git clone https://gitlab.com/FirstBloodRPG/config  ..
-git subtree -P config pull ../clone master
-git subtree -P config push ../clone aif
+git subtree -P config pull ../config master
+git subtree -P config push ../config aif
 pdflatex -shell-escape main.tex
 makeglossaries main
 makeglossaries main.idx
