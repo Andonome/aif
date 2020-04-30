7b72a9d30c54203b98fb53096ad884fa0255396b
Malin Freeborn <malinfreeborn@tutamail.com>
2020-04-30 14:27:25 +0200

    make autocompile a symbolic link

--- autocompile.sh
+++ autocompile.sh
@@ -1,15 +1,1 @@
-#!/bin/bash
-if [ $1 = clean ]; then
-	rm -r *aux *log main.{i,g}* *toc *.ptc *.out svg-inkscape 2>/dev/null
-	exit 0
-fi
-
-if [ $1 = tree ]; then
-	git status
-	git subtree -P config pull ../config master
-	git subtree -P config push ../config aif
-fi
-pdflatex -shell-escape main.tex
-makeglossaries main
-makeindex main.idx
-pdflatex main.tex
+config/autocompile.sh
